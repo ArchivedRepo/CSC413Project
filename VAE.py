d157f0700193ba883f0996cbca6cb5041f99352d
Qingyuan Qie <willqie@gmail.com>
2022-04-09 18:53:53 -0400

    add VAE

---
+++ VAE.py
@@ -0,0 +1,80 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Encoder(nn.Module):
+    def __init__(self, capacity, latent_dims):
+        super(Encoder, self).__init__()
+        c = capacity
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=c, kernel_size=4, stride=2, padding=1)  # out: c x 64 x 64
+        self.conv2 = nn.Conv2d(in_channels=c, out_channels=c * 2, kernel_size=4, stride=2,
+                               padding=1)  # out: c x 32 x 32
+        self.fc_mu = nn.Linear(in_features=c * 2 * 32 * 32, out_features=latent_dims)
+        self.fc_logvar = nn.Linear(in_features=c * 2 * 32 * 32, out_features=latent_dims)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = x.view(x.size(0), -1)  # flatten batch of multi-channel feature maps to a batch of feature vectors
+        x_mu = self.fc_mu(x)
+        x_logvar = self.fc_logvar(x)
+        return x_mu, x_logvar
+
+
+class Decoder(nn.Module):
+    def __init__(self, capacity, latent_dims):
+        super(Decoder, self).__init__()
+        c = capacity
+        self.fc = nn.Linear(in_features=latent_dims, out_features=c * 2 * 32 * 32)
+        self.conv2 = nn.ConvTranspose2d(in_channels=c * 2, out_channels=c, kernel_size=4, stride=2, padding=1)
+        self.conv1 = nn.ConvTranspose2d(in_channels=c, out_channels=3, kernel_size=4, stride=2, padding=1)
+
+    def forward(self, x):
+        x = self.fc(x)
+        x = x.view(x.size(0), self.capacity * 2, 32,
+                   32)  # unflatten batch of feature vectors to a batch of multi-channel feature maps
+        x = F.relu(self.conv2(x))
+        x = torch.sigmoid(
+            self.conv1(x))  # last layer before output is sigmoid, since we are using BCE as reconstruction loss
+        return x
+
+
+class VariationalAutoencoder(nn.Module):
+    def __init__(self, capacity, latent_dim):
+        super(VariationalAutoencoder, self).__init__()
+        self.encoder = Encoder(capacity, latent_dim)
+        self.decoder = Decoder(capacity, latent_dim)
+
+    def forward(self, x):
+        latent_mu, latent_logvar = self.encoder(x)
+        latent = self.latent_sample(latent_mu, latent_logvar)
+        x_recon = self.decoder(latent)
+        return x_recon, latent_mu, latent_logvar
+
+    def latent_sample(self, mu, logvar):
+        if self.training:
+            # the reparameterization trick
+            std = logvar.mul(0.5).exp_()
+            eps = torch.empty_like(std).normal_()
+            return eps.mul(std).add_(mu)
+        else:
+            return mu
+
+
+def vae_loss(recon_x, x, mu, logvar):
+    # recon_x is the probability of a multivariate Bernoulli distribution p.
+    # -log(p(x)) is then the pixel-wise binary cross-entropy.
+    # Averaging or not averaging the binary cross-entropy over all pixels here
+    # is a subtle detail with big effect on training, since it changes the weight
+    # we need to pick for the other loss term by several orders of magnitude.
+    # Not averaging is the direct implementation of the negative log likelihood,
+    # but averaging makes the weight of the other loss term independent of the image resolution.
+    recon_loss = F.binary_cross_entropy(recon_x.view(-1, 16384), x.view(-1, 16384), reduction='sum')
+
+    # KL-divergence between the prior distribution over latent vectors
+    # (the one we are going to sample from when generating new images)
+    # and the distribution estimated by the generator for the given image.
+    kldivergence = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+
+    return recon_loss + kldivergence
---
+++ VAE_train.py
@@ -0,0 +1,61 @@
+import torch
+from VAE import VariationalAutoencoder, vae_loss
+
+
+latent_dims = 20
+num_epochs = 3
+batch_size = 64
+capacity = 64
+learning_rate = 1e-3
+use_gpu = True
+log_step = 10
+losses = {"iteration": [], "loss": []}
+
+vae = VariationalAutoencoder(capacity, latent_dims)
+
+device = torch.device("cuda:0" if use_gpu and torch.cuda.is_available() else "cpu")
+vae = vae.to(device)
+
+num_params = sum(p.numel() for p in vae.parameters() if p.requires_grad)
+print('Number of parameters: %d' % num_params)
+
+optimizer = torch.optim.Adam(params=vae.parameters(), lr=learning_rate, weight_decay=1e-5)
+
+# set to training mode
+vae.train()
+
+train_loss_avg = []
+
+print('Training ...')
+for epoch in range(num_epochs):
+    train_loss_avg.append(0)
+    num_batches = 0
+
+    for image_batch, _ in train_dataloader:
+
+        image_batch = image_batch.to(device)
+
+        # vae reconstruction
+        image_batch_recon, latent_mu, latent_logvar = vae(image_batch)
+
+        # reconstruction error
+        loss = vae_loss(image_batch_recon, image_batch, latent_mu, latent_logvar)
+
+        # backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+
+        # one step of the optmizer (using the gradients from backpropagation)
+        optimizer.step()
+
+        train_loss_avg[-1] += loss.item()
+
+        if num_batches % log_step == 0:
+            losses['iteration'].append(num_batches)
+            losses['loss'].append(loss.item())
+            print('Iteration [{:4d}/{:4d}] | train_loss: {:6.4f}'.format(
+                num_batches, 100, loss.item()))
+        num_batches += 1
+
+    train_loss_avg[-1] /= num_batches
+    print('Epoch [%d / %d] average reconstruction error: %f' % (epoch + 1, num_epochs, train_loss_avg[-1]))
