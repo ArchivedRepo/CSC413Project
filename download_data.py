5f072991081a5edb82ce068a4b62942ff37768d1
Qingyuan Qie <willqie@gmail.com>
2022-04-09 18:21:36 -0400

    download data script

---
+++ download_data.py
@@ -0,0 +1,27 @@
+import wget
+import os.path
+import zipfile
+
+def get_file(fname, origin, untar=False, cache_dir='data'):
+    data_dir = os.path.join(cache_dir)
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+
+    fpath = os.path.join(data_dir, fname)
+
+    print(fpath)
+    if not os.path.exists(fpath):
+        print('Downloading data from', origin)
+
+        try:
+            wget.download(fpath, origin)
+        except (Exception, KeyboardInterrupt) as e:
+            if os.path.exists(fpath):
+                os.remove(fpath)
+            print("Download Aborted!")
+    return fpath
+
+
+def unzip_dataset(fpath, target_path):
+    with zipfile.ZipFile(fpath) as myzip:
+        myzip.extractall(target_path)
